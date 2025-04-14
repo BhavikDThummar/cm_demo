4e018ad0c62a873e5e129dd77ca575cc8a20a5eb
Bhavik <bhavik.thummar@triveniglobalsoft.com>
2025-04-14 11:50:22 +0000

    Added Component and Basic BOM tables.

---
+++ cm_demo/cm_demo/doctype/cm_rfq_lineitems/cm_rfq_lineitems.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, BT and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("CM RFQ Lineitems", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ cm_demo/cm_demo/doctype/cm_rfq_lineitems_alternate_part/cm_rfq_lineitems_alternate_part.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, BT and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class CMRFQLineitemsAlternatePart(Document):
+	pass
